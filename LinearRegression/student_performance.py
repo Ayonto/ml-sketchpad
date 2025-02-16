bf203037b0bd649b0b82b29c3c39b10c78cb61ba
Ayonto <s.i.ayonto@gmail.com>
2025-02-16 23:44:08 +0600

    tested multi linear regression with student performace dataset

--- LinearRegression/student_performance.py
+++ LinearRegression/student_performance.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression as LR
 
-df = pd.read_csv('./Student_Performance.csv')
+df = pd.read_csv('LinearRegression/Student_Performance.csv')
 
 # Converting "Yes" to 1 and "No" to 0
 df["Extracurricular Activities"] = df["Extracurricular Activities"].replace({"Yes": 1, "No": 0})
@@ -24,7 +24,7 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
 
 # model fitting
 model = LinearRegression()
-model.fit(x_train, y_train, learning_rate=0.00003, iters = 1000)
+model.fit(x_train, y_train, learning_rate=0.00003, iters = 10001)
 
 print("Model Score: ", model.score(x_test, y_test))
 
