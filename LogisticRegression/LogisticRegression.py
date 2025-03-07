0c659822ece0dc9e41464420eb98707b0358d50f
Ayonto <s.i.ayonto@gmail.com>
2025-03-07 15:47:04 +0600

    updated

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -1,12 +1,91 @@
 import numpy as np 
+import matplotlib.pyplot as plt
 
 class LogisticRegression: 
     def __init__(self): 
         self.w = None
         self.b = None
 
-    def cost_function(self): 
-        pass
+        self.J = []
+        self.iters = []
+    
+    def sigmoid(self, x, w, b): 
+        return 1 / (1 + np.exp(-(np.dot(x, w) + b)))
 
-    def fit(self, X, Y): 
-        pass
+    def cost_function(self, X, Y, w, b): 
+        m = X.shape[0]
+        cost = 0
+        for i in range(m): 
+            z_i = np.dot(X[i], w) + b
+
+            f_wb_i = self.sigmoid(X[i], w, b)
+            cost += -Y[i] * np.log(f_wb_i) - (1 - Y[i]) * np.log(1-f_wb_i)
+
+        cost = cost / m
+        return cost
+    
+    def gradient(self, X, Y, w, b): 
+        m, n = X.shape
+        dj_dw = np.zeros(n)
+        dj_db = 0
+        # print(f"Debug: m {m}")
+        for i in range(m): 
+            f_wb = self.sigmoid(X[i], w, b)
+            dj_dw = (f_wb - Y[i] ) * X[i]
+            dj_db = f_wb - Y[i]
+            dj_dw += dj_dw
+            dj_db += dj_db
+
+        dj_dw /= m
+        dj_db /= m
+        return dj_dw, dj_db
+
+    def fit(self, X, Y, learning_rate, iters=1000): 
+        m, n = X.shape
+        w = np.zeros((n,))
+        b = 0
+
+        for i in range(iters): 
+            dj_dw, dj_db = self.gradient(X, Y, w, b)
+
+
+            w = w - learning_rate * dj_dw
+            b = b - learning_rate * dj_db
+
+            if i % 100 == 0:
+                print(f"Iteration {i}: w = {w}, b = {b}, Cost: {self.cost_function(X, Y, w, b)}")
+            if i% 10 == 0:
+                self.J.append(self.cost_function(X, Y, w, b))
+
+                self.iters.append(i)
+
+        self.w = w
+        self.b = b
+
+    def show_learning_curve(self): 
+        plt.plot(self.iters, self.J, marker = 'o', linestyle='-')
+        plt.xlabel("Iterations")
+        plt.ylabel("Cost Function J(w, b)")
+        plt.title("Learning Curve")
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
+    def score(self, X_test, Y_test):
+            # Predict using the trained model
+            Y_pred = self.predict(X_test)
+
+            # Compute R² Score
+            ss_total = np.sum((Y_test - np.mean(Y_test)) ** 2)  # Total Sum of Squares
+            ss_residual = np.sum((Y_test - Y_pred) ** 2)  # Residual Sum of Squares
+
+            r2_score = 1 - (ss_residual / ss_total)
+
+            return r2_score
+    def predict(self, X): 
+        print(f"shape of X: {X.shape}")
+        return self.sigmoid(X, self.w, self.b)
+
+            
+
+    
--- LogisticRegression/main.py
+++ LogisticRegression/main.py
@@ -11,17 +11,24 @@ data["Helmet_Used"] = data["Helmet_Used"].map({"Yes": 1, "No": 0})
 data["Seatbelt_Used"] = data["Seatbelt_Used"].map({"Yes": 1, "No": 0})
 data["Gender"] = data["Gender"].map({"Male": 1, "Female": 0})
 
-Y = data["Survived"]
-X = data.drop(["Survived"], axis=1)
+Y = data["Survived"].to_numpy()
+X = data.drop(["Survived"], axis=1).to_numpy()
 
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
 
-print(x_train)
+# print(x_train)
+
 
 model = LogisticRegression()
 
-model.fit(x_train, y_train)
+model.fit(x_train, y_train, learning_rate = 0.000001, iters = 1000)
+
+result = model.predict(x_test)
+print(f"score : {model.score(x_test, y_test)}")
+model.show_learning_curve()
+print(result)
+print(y_test)
 
 
 
